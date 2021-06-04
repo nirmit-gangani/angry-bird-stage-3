e8d2ffbf03efa336d4e32e2a587f2b9c8f2d49f3
nirmit-gangani <nirmitgangani@gmail.com>
2021-06-04 03:16:55 -0700

    test

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -33,9 +34,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(230,180,80, PI/2);
 
     bird = new Bird(100,100);
 
+    slingshot= new Slingshot(bird.body,log6.body);
 }
 
 function draw(){
@@ -58,7 +61,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
+
+    slingshot.display();
 }
